69e7afd65d60fb953cbcb33151a52ae33be19a33
Alexander Liu <a@alexanderliu.com>
2023-08-12 16:22:45 -0700

    style: run prettier

--- apps/sanity/sanity.cli.ts
+++ apps/sanity/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
-  api: {
-    projectId: '37wc8wys',
-    dataset: 'production'
-  }
-})
+	api: {
+		projectId: "37wc8wys",
+		dataset: "production",
+	},
+});
