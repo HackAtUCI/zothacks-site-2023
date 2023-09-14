01cc7c4fb9be85ec0014cfc30ac5352b21e00822
Tyler Yu <tyleryy@uci.edu>
2023-09-13 22:18:49 -0700

    feat: draggable sticker component

--- apps/site/src/views/Landing/Landing.tsx
+++ apps/site/src/views/Landing/Landing.tsx
@@ -1,9 +1,16 @@
+"use client";
+
 import "./Landing.module.scss";
+import Sticker from "@/components/Sticker";
+import HackLogo from "@/assets/icons/hack.png";
 
 export default function Landing() {
 	return (
 		<main>
 			<h1>ZotHacks 2023</h1>
+			<div style={{ marginTop: 100, position: "relative" }}>
+				<Sticker imageSrc={HackLogo.src} alt="test" />
+			</div>
 		</main>
 	);
 }
