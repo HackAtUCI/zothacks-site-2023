7ac6d224974ec0ebfffec05c3a684fb65bdb0b31
Nathan Huey <nate.huey4@gmail.com>
2023-09-13 23:11:24 -0700

    feat: faq structure and styling

--- apps/site/src/views/Home/sections/FAQ/FAQ.tsx
+++ apps/site/src/views/Home/sections/FAQ/FAQ.tsx
@@ -1,11 +1,52 @@
+import Image from "next/image";
 import Container from "react-bootstrap/Container";
+import Accordion from "react-bootstrap/Accordion";
+import styles from "./FAQ.module.scss";
 
-function FAQ() {
+import star from "@/assets/images/star.png";
+import eraser from "@/assets/images/eraser.png";
+import { client } from "@/lib/sanity/client";
+
+interface sanityResponse {
+	faqs: faqType[];
+}
+
+interface faqType {
+	_key: string;
+	_type: string;
+	question: string;
+	answer: string;
+}
+
+export default async function FAQ() {
+	// const data: sanityResponse[] = await client.fetch('*[_type == "faqs"]');
+	// const faq: faqType[] = data[0]["faqs"];
+
 	return (
 		<Container as="section">
-			<h2>FAQ</h2>
+			<div className={styles.faq}>
+				<div className={styles["vertical-line"]} />
+				<Image src={star} alt="star" className={styles["star-lg"]} />
+				<Image src={star} alt="star" className={styles["star-sm"]} />
+				<Image src={eraser} alt="eraser" className={styles["eraser"]} />
+				<h2 className={styles.title}>FAQ</h2>
+				<Accordion className={styles.accordion} alwaysOpen>
+					{/* {faq.map((q, index) => (
+						<Accordion.Item
+							key={index}
+							className={styles["accordion-border"]}
+							eventKey={`${index}`}
+						>
+							<Accordion.Header as="h3" className={styles.body}>
+								<strong>{q["question"]}</strong>
+							</Accordion.Header>
+							<Accordion.Body as="p" className={styles.body}>
+								{q["answer"]}
+							</Accordion.Body>
+						</Accordion.Item>
+					))} */}
+				</Accordion>
+			</div>
 		</Container>
 	);
 }
-
-export default FAQ;
