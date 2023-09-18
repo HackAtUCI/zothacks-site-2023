e9b8016ce54062d7b80c8d6a3a6ccc9e979c6233
Tyler Yu <tyleryy@uci.edu>
2023-09-18 03:21:39 -0700

    feat: draggable sticker enhancements
    feat: sticker dragConstraints and elastic
    
    added type
    
    sticker spring transition
    
    reduced spring stiffness
    
    shadow adjustments
    
    feat: base sticker component
    
    individual sticker example
    
    rename to Base Sticker
    
    imports

--- apps/site/src/views/Home/sections/Landing/Landing.tsx
+++ apps/site/src/views/Home/sections/Landing/Landing.tsx
@@ -1,11 +1,15 @@
 import ApplyButton from "@/views/Home/components/ApplyButton/ApplyButton";
-
+import Sticker from "@/components/Sticker/Sticker";
+import HackLogo from "@/assets/icons/hack.png";
 import styles from "./Landing.module.scss";
 
 function Landing() {
 	return (
 		<div className={styles.landing}>
 			<h1>ZotHacks 2023</h1>
+			<div style={{ marginTop: 100, position: "relative" }}>
+				<Sticker imageSrc={HackLogo.src} alt="test" draggable />
+			</div>
 			<ApplyButton />
 		</div>
 	);
