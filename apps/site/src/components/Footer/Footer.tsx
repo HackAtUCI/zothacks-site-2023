e86a82acbfb7c42a28773203b87bdbe0db34b98e
Albert Wang <alberw5@uci.edu>
2023-09-27 00:40:16 -0700

    update: icons look like stickers with a shadow

--- apps/site/src/components/Footer/Footer.tsx
+++ apps/site/src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import styles from "./Footer.module.scss";
 
 import Mail from "@/assets/icons/email.svg";
 import Facebook from "@/assets/icons/facebook.png";
-import Instagram from "@/assets/icons/instagram.png";
+import Instagram from "@/assets/icons/instagram.svg";
 import Hack from "@/assets/icons/hack-logo.svg";
 import TikTok from "@/assets/icons/tiktok.png";
 
@@ -14,8 +14,6 @@ type Social = {
 	icon: any;
 	link: string;
 	alt: string;
-	width: number;
-	height: number;
 };
 
 const SOCIALS: Social[] = [
@@ -23,36 +21,26 @@ const SOCIALS: Social[] = [
 		icon: Hack,
 		link: "https://hack.ics.uci.edu/",
 		alt: "Hack at UCI",
-		width: 59,
-		height: 59,
 	},
 	{
 		icon: Mail,
 		link: "mailto:hack@uci.edu",
 		alt: "Mail",
-		width: 59,
-		height: 59,
 	},
 	{
 		icon: Facebook,
 		link: "https://www.facebook.com/UCI.Hack/",
 		alt: "Facebook",
-		width: 59,
-		height: 59,
 	},
 	{
 		icon: Instagram,
 		link: "https://www.instagram.com/hackatuci/",
 		alt: "Instagram",
-		width: 59,
-		height: 59,
 	},
 	{
 		icon: TikTok,
 		link: "https://www.tiktok.com/@hackatuci",
 		alt: "TikTok",
-		width: 59,
-		height: 59,
 	},
 ];
 
@@ -61,13 +49,13 @@ export default function Footer() {
 		<footer className={styles.footer}>
 			<p className={styles.text}>Made with &lt;3 in Irvine, CA - Hack @ UCI</p>
 			<div className="text-center">
-				{SOCIALS.map(({ icon, link, alt, width, height }) => (
-					<a key={link} href={link} target="_blank" className={styles.socials}>
+				{SOCIALS.map(({ icon, link, alt }) => (
+					<a key={link} href={link} target="_blank">
 						<Image
 							src={icon.src}
 							alt={alt}
-							width={width}
-							height={height}
+							width={45}
+							height={45}
 							className={styles.socials}
 						/>
 					</a>
