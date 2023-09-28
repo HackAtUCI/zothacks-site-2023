cc9ebf846546e5522d1ffc5ccf9297cb6b12cd9c
Tyler Yu <tyleryy@uci.edu>
2023-09-27 23:00:48 -0700

    fix: removed hack sticker and adjusted heart sticker position

--- apps/site/src/views/Home/components/StickerLayout/StickerLayout.tsx
+++ apps/site/src/views/Home/components/StickerLayout/StickerLayout.tsx
@@ -3,19 +3,11 @@ import { HackSticker, HeartSticker } from "@/components/Sticker/Stickers";
 export default function StickerLayout() {
 	return (
 		<div style={{ maxWidth: "100vw" }}>
-			<HackSticker
-				style={{
-					position: "absolute",
-					left: "60vw",
-					top: "15vh",
-					zIndex: 100,
-				}}
-			/>
 			<HeartSticker
 				style={{
 					position: "absolute",
-					left: "30vw",
-					top: "15vh",
+					left: "52vw",
+					top: "35vh",
 					zIndex: 100,
 				}}
 			/>
