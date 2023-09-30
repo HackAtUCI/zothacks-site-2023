a7bd96dc1ce9d29342752b72bf70f4217a7b0117
Tyler Yu <tyleryy@uci.edu>
2023-09-30 00:25:18 -0700

    feat: backend resources section
    feat: backend card
    
    fix: removed console log
    
    feat: flask sticky note and config setup
    
    feat: Backend Resource section

---
+++ apps/site/src/views/Resources/components/Backend_group/Backend_group.tsx
@@ -0,0 +1,61 @@
+import styles from "./Backend_group.module.scss";
+
+interface tag {
+	link: string;
+	src: string;
+	alt: string;
+	className?: string;
+}
+
+interface Backend_Group_props {
+	card: string | undefined;
+	title: string | undefined;
+	description: string | undefined;
+	tagList: tag[] | undefined;
+	className?: string | undefined;
+}
+
+export default function Backend_Group({
+	card,
+	title,
+	description,
+	tagList,
+	className,
+}: Backend_Group_props) {
+	return (
+		<>
+			{/* height hardcoded for 3 tags */}
+			<div
+				style={{
+					backgroundImage: `url(${card})`,
+					backgroundSize: "cover",
+					backgroundRepeat: "no-repeat",
+					width: "495px",
+					height: "510px",
+					position: "relative",
+				}}
+				className={className}
+			>
+				<div className={styles.text_flexbox}>
+					<h2 className={styles.title}>{title}</h2>
+					<p className={styles.description}>{description}</p>
+				</div>
+				<div className={styles.tag_flexbox}>
+					{tagList?.map((tag) => (
+						<Resource_Tag link={tag.link} src={tag.src} alt={tag.alt} />
+					))}
+				</div>
+			</div>
+		</>
+	);
+}
+
+function Resource_Tag({ link, className, src, alt }: tag) {
+	return (
+		<div className={className}>
+			<a href={link}>
+				<img src={src} className={styles.tag} alt={alt} />
+			</a>
+		</div>
+	);
+}
