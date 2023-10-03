df36eaa8aa2de0f1c9d22c45418818f53015278e
Tyler Yu <tyleryy@uci.edu>
2023-10-03 12:58:05 -0700

    update: redirect in next config

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -6,6 +6,20 @@ const nextConfig = {
 	sassOptions: {
 		includePaths: [path.join(__dirname, "src", "lib", "styles")],
 	},
+	async redirects() {
+		return [
+			{
+				source: "/",
+				destination: "/home",
+				permanent: true,
+			},
+			{
+				source: "/apply",
+				destination: "https://forms.gle/nxRU44y9Gd5mrmXUA",
+				permanent: true,
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
--- apps/site/src/app/apply/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function ApplyRedirectRoute() {
-	redirect(
-		`https://docs.google.com/forms/d/e/1FAIpQLSethOFfe_PSRl587ZKPrhQEOIi_nz2s1B8wDJAzQj0r3UebnQ/viewform`,
-	);
-}
