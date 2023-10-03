9df165c5f1834d6df793b95bc4f36ce9532ba0e7
Nathan Huey <nate.huey4@gmail.com>
2023-10-03 14:53:08 -0700

    feat: fetch faq data from sanity

--- apps/site/src/views/Home/sections/FAQ/FAQ.tsx
+++ apps/site/src/views/Home/sections/FAQ/FAQ.tsx
@@ -1,52 +1,31 @@
+import { getQuestions } from "./getQuestions";
+import FAQAccordion from "./FAQAccordion";
+
 import Image from "next/image";
-import Container from "react-bootstrap/Container";
-import Accordion from "react-bootstrap/Accordion";
+import { PortableText } from "@portabletext/react";
 import styles from "./FAQ.module.scss";
 
 import star from "@/assets/images/star.png";
 import eraser from "@/assets/images/eraser.png";
-import { client } from "@/lib/sanity/client";
-
-interface sanityResponse {
-	faqs: faqType[];
-}
-
-interface faqType {
-	_key: string;
-	_type: string;
-	question: string;
-	answer: string;
-}
 
 export default async function FAQ() {
-	// const data: sanityResponse[] = await client.fetch('*[_type == "faqs"]');
-	// const faq: faqType[] = data[0]["faqs"];
+	const questions = await getQuestions();
+	const faq = questions[0]["faqs"].map(({ _key, question, answer }) => ({
+		_key: _key,
+		question: <strong>{question}</strong>,
+		answer: <PortableText value={answer} />,
+	}));
 
 	return (
-		<Container as="section">
+		<section className={styles.container}>
 			<div className={styles.faq}>
 				<div className={styles["vertical-line"]} />
 				<Image src={star} alt="star" className={styles["star-lg"]} />
 				<Image src={star} alt="star" className={styles["star-sm"]} />
 				<Image src={eraser} alt="eraser" className={styles["eraser"]} />
 				<h2 className={styles.title}>FAQ</h2>
-				<Accordion className={styles.accordion} alwaysOpen>
-					{/* {faq.map((q, index) => (
-						<Accordion.Item
-							key={index}
-							className={styles["accordion-border"]}
-							eventKey={`${index}`}
-						>
-							<Accordion.Header as="h3" className={styles.body}>
-								<strong>{q["question"]}</strong>
-							</Accordion.Header>
-							<Accordion.Body as="p" className={styles.body}>
-								{q["answer"]}
-							</Accordion.Body>
-						</Accordion.Item>
-					))} */}
-				</Accordion>
+				<FAQAccordion faq={faq} />
 			</div>
-		</Container>
+		</section>
 	);
 }
