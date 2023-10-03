41a2c7fc4058caec9acd80d1064ccb1c0ff51e51
Tyler Yu <tyleryy@uci.edu>
2023-10-03 01:24:40 -0700

    feat: maintenance mode

--- apps/site/src/views/Schedule/Schedule.tsx
+++ apps/site/src/views/Schedule/Schedule.tsx
@@ -1,5 +1,18 @@
+import maintenanceImage from "@/assets/images/maintenance.png";
+import comingSoonImage from "@/assets/images/coming_soon.jpg";
 import "./Schedule.module.scss";
 
 export default function Schedule() {
-	return <h1>Schedule</h1>;
+	return (
+		<div
+			style={{
+				width: "100vw",
+				height: `${comingSoonImage.height}px`,
+				backgroundImage: `url(${comingSoonImage.src})`,
+				backgroundRepeat: "no-repeat",
+				backgroundPosition: "center",
+				backgroundSize: "center",
+			}}
+		></div>
+	);
 }
