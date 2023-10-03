1fea5b1ea3eb0254b80e252afc0dc226ce705bcf
Tyler Yu <tyleryy@uci.edu>
2023-10-03 00:59:58 -0700

    fix: altered Bookmark Link prop to accept variable props

--- apps/site/src/components/BookmarkLink/BookmarkLink.tsx
+++ apps/site/src/components/BookmarkLink/BookmarkLink.tsx
@@ -6,18 +6,21 @@ import styles from "./BookmarkLink.module.scss";
 interface BookmarkLinkProps {
 	className?: string;
 	href: string;
+	props: any;
 }
 
 export default function BookmarkLink({
 	className,
 	href,
 	children,
+	...props
 }: PropsWithChildren<BookmarkLinkProps>) {
 	return (
 		<Button
 			className={styles.bookmarkLink + " " + className}
 			variant=""
 			href={href}
+			{...props}
 		>
 			{children}
 		</Button>
