6d95be1904cb293a1bd4ae9dbfa51215e5933115
Tyler Yu <tyleryy@uci.edu>
2023-10-05 03:07:43 -0700

    fix: removed mentor folder

--- apps/site/src/app/mentor/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function MentorRedirectRoute() {
-	redirect(
-		"https://docs.google.com/forms/d/e/1FAIpQLSexqsA7DXk9_c2N3s1FjJHMJLNiYsc1LlSo3-7bYNlOpWFYLg/viewform",
-	);
-}
