cfe2f338a0cc6c070328f476f5104e9cd557f37f
Nathan Huey <nate.huey4@gmail.com>
2023-10-07 22:35:32 -0700

    fix: change schedule schema to use different documents

--- apps/sanity/schemas/schedule.ts
+++ apps/sanity/schemas/schedule.ts
@@ -1,4 +1,4 @@
-import { defineType, defineField, defineArrayMember } from "sanity";
+import { defineType, defineField} from "sanity";
 
 export default defineType({
 	name: "schedule",
@@ -6,47 +6,34 @@ export default defineType({
 	type: "document",
 	fields: [
 		defineField({
-			name: "schedule",
-			title: "Schedule",
-			type: "array",
-			of: [
-				defineArrayMember({
-					type: "object",
-					name: "event",
-					fields: [
-						defineField({
-							name: "title",
-							title: "title",
-							type: "string"
-						}),
-						defineField({
-							name: "startTime",
-							title: "startTime",
-							type: "datetime"
-						}),
-						defineField({
-							name: "endTime",
-							title: "endTime",
-							type: "datetime"
-						}),
-						defineField({
-							name: "category",
-							title: "category",
-							type: "string"
-						}),
-						defineField({
-							name: "host",
-							title: "host",
-							type: "string"
-						}),
-						defineField({
-							name: "description",
-							title: "description",
-							type: "text"
-						})
-					]
-				})
-			]
+			name: "title",
+			title: "title",
+			type: "string"
+		}),
+		defineField({
+			name: "startTime",
+			title: "startTime",
+			type: "datetime"
+		}),
+		defineField({
+			name: "endTime",
+			title: "endTime",
+			type: "datetime"
+		}),
+		defineField({
+			name: "category",
+			title: "category",
+			type: "string"
+		}),
+		defineField({
+			name: "host",
+			title: "host",
+			type: "string"
+		}),
+		defineField({
+			name: "description",
+			title: "description",
+			type: "text"
 		})
 	],
 });
