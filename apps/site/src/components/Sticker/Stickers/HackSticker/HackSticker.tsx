ff15acfb07ed1a9a3879d615f92527bf66b3db50
Alexander Liu <a@alexanderliu.com>
2023-10-29 14:41:51 -0700

    feature: Sticker Positioning (#76)
    * update: removed deadzone
    
    * feat: position stickers relative to page elements
    
    * refactor: centralize sticker positioning logic to base sticker component
    
    * fix: position value wrapped with quotes
    
    * refactor: remove unnecessary forwardRef on sticker
    
    * feat: sticker positioning component
    
    * fix: remove extraneous broken sticker layout component
    
    * feat: remove hack sticker from button
    
    ---------
    
    Co-authored-by: Tyler Yu <tyleryy@uci.edu>

--- apps/site/src/components/Sticker/Stickers/HackSticker/HackSticker.tsx
+++ apps/site/src/components/Sticker/Stickers/HackSticker/HackSticker.tsx
@@ -1,18 +1,18 @@
+import type React from "react";
+import type { StickerProps } from "../stickerProps";
 import HackLogo from "@/assets/icons/hack.png";
 import BaseSticker from "../../BaseSticker";
-import styles from "./HackSticker.module.scss";
 import { lightShake } from "@/components/animation";
 
-export default function HackSticker({ style }: { style?: object | undefined }) {
-	return (
-		<div className={styles.stickerContainer} style={{ ...style }}>
-			<BaseSticker
-				imageSrc={HackLogo.src}
-				alt="Hack at UCI sticker"
-				height={200}
-				width={200}
-				{...lightShake}
-			/>
-		</div>
-	);
-}
+const HackSticker: React.FC<StickerProps> = (props) => (
+	<BaseSticker
+		imageSrc={HackLogo.src}
+		alt="Hack at UCI sticker"
+		height={200}
+		width={200}
+		{...lightShake}
+		{...props}
+	/>
+);
+
+export default HackSticker;
