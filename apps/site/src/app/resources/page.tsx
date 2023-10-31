52e0d786a6729bd4cab28ba29d28d937ce28cd44
Taesung Hwang <taesungh@uci.edu>
2023-10-30 20:15:15 -0700

    Configure maintenance mode from environment
    - Show Maintenance view instead of page content when environment
      variable is set for Schedule and Resources page

--- apps/site/src/app/resources/page.tsx
+++ apps/site/src/app/resources/page.tsx
@@ -1,1 +1,9 @@
-export { Resources as default } from "@/views";
+import { Resources } from "@/views";
+import { Maintenance } from "@/views";
+
+// When set to any value
+const Component = process.env.MAINTENANCE_MODE_RESOURCES
+	? Maintenance
+	: Resources;
+
+export default Component;
--- apps/site/src/app/schedule/page.tsx
+++ apps/site/src/app/schedule/page.tsx
@@ -1,1 +1,9 @@
-export { Schedule as default } from "@/views";
+import { Schedule } from "@/views";
+import { Maintenance } from "@/views";
+
+// When set to any value
+const Component = process.env.MAINTENANCE_MODE_SCHEDULE
+	? Maintenance
+	: Schedule;
+
+export default Component;
