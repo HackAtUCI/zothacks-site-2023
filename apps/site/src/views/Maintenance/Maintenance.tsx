3eaced53dfbb735e1ee065dbed303e93d3eef1e4
Taesung Hwang <taesungh@uci.edu>
2023-10-30 20:04:02 -0700

    Simplify maintenance image layout & use next/image

--- apps/site/src/views/Maintenance/Maintenance.tsx
+++ apps/site/src/views/Maintenance/Maintenance.tsx
@@ -5,27 +5,18 @@ import styles from "./Maintenance.module.scss";
 
 function Maintenance() {
 	return (
-		<div className="container" style={{ width: "100vw", height: "80vh" }}>
-			<div
-				className={styles.maintenanceContainer}
-				style={{
-					position: "absolute",
-					top: "50%",
-					left: "50%",
-					transform: "translate(-50%, -50%)",
-				}}
-			>
-				<img
+		<section className={"container " + styles.maintenanceContainer}>
+			<div className={styles.imageContainer}>
+				<Image
 					src={maintenancePetr.src}
 					alt="Anteaters working at a construction site."
-					style={{ width: "100%" }}
+					fill
+					style={{ objectFit: "contain" }}
 				/>
-				<div className="text-center">
-					<h2>Page Under Maintenance</h2>
-					<p>Check back soon!</p>
-				</div>
 			</div>
-		</div>
+			<h1 className="fs-2">Page Under Maintenance</h1>
+			<p>Check back soon!</p>
+		</section>
 	);
 }
 
