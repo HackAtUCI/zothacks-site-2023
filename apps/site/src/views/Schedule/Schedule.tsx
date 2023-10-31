3f0fc0ec5c4ce86f938855b9c8817f24425a1d09
Alexander Liu <a@alexanderliu.com>
2023-10-31 05:13:05 -0700

    Feature: Useable Events / Schedule Page (#106)
    * update: created getSchedule file
    
    * update: synced sanity data with schedule component
    
    * feat: update event schema
    
    * feat: group events by day
    
    * feat: format date ranges
    
    * fix: group events by America/Los_Angeles day
    
    * fix: event order asc
    
    * fix: remove resources from nav
    
    ---------
    
    Co-authored-by: Tyler Yu <tyleryy@uci.edu>

--- apps/site/src/views/Schedule/Schedule.tsx
+++ apps/site/src/views/Schedule/Schedule.tsx
@@ -1,23 +1,23 @@
 import ClipboardSchedule from "./sections/ClipboardSchedule/ClipboardSchedule";
+import { getSchedule } from "./getSchedule";
+import { PortableText } from "@portabletext/react";
 
 import styles from "./Schedule.module.scss";
 
-export default function Schedule() {
+export default async function Schedule() {
+	const days = await getSchedule();
+
+	const schedule = days.map((events) =>
+		events.map(({ description, ...event }) => ({
+			...event,
+			description: <PortableText value={description} />,
+		})),
+	);
+
 	return (
 		<div className={styles.schedule}>
 			<h1>Schedule</h1>
-			<ClipboardSchedule
-				schedule={[
-					{
-						_key: "test",
-						title: "test",
-						description: "test",
-						location: "test",
-						startDate: new Date(),
-						endDate: new Date(),
-					},
-				]}
-			/>
+			<ClipboardSchedule schedule={schedule} />
 		</div>
 	);
 }
--- apps/site/src/views/Schedule/sections/ClipboardSchedule/ClipboardSchedule.tsx
+++ apps/site/src/views/Schedule/sections/ClipboardSchedule/ClipboardSchedule.tsx
@@ -4,8 +4,6 @@ import Image from "next/image";
 
 import Accordion from "react-bootstrap/Accordion";
 import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 
 import clip from "@/assets/images/clip.svg";
 
@@ -13,14 +11,21 @@ import styles from "./ClipboardSchedule.module.scss";
 
 interface ClipboardScheduleProps {
 	schedule: {
-		_key: string;
 		title: string;
-		description: string;
-		location: string;
-		startDate: Date;
-		endDate: Date;
-	}[];
+		location?: string | undefined;
+		virtual?: string | undefined;
+		startTime: Date;
+		endTime: Date;
+		organization?: string | undefined;
+		hosts?: string[] | undefined;
+		description: JSX.Element;
+	}[][];
 }
+
+const dateTimeFormat = new Intl.DateTimeFormat("en", {
+	hour: "numeric",
+	minute: "numeric",
+});
 
 function ClipboardSchedule({ schedule }: ClipboardScheduleProps) {
 	return (
@@ -31,28 +36,33 @@ function ClipboardSchedule({ schedule }: ClipboardScheduleProps) {
 			<div className={styles.clip}>
 				<Image src={clip} alt="Clipboard clip" className={styles.clip} />
 			</div>
-			<h2 className="mb-5">Countdown Timer</h2>
 			<Accordion defaultActiveKey="0" className={styles.accordion}>
-				{schedule.map(
-					(
-						{ _key, title, description, location, startDate, endDate },
-						index,
-					) => (
-						<Accordion.Item
-							key={_key}
-							eventKey={`${index}`}
-							className={styles.accordionItem}
-						>
-							<Accordion.Header className={styles.accordionHeader}>
-								<h3>{title}</h3>
-								<span className="text-end ms-auto">
-									{location}, {startDate.toString()} - {endDate.toString()}
-								</span>
-							</Accordion.Header>
-							<Accordion.Body>{description}</Accordion.Body>
-						</Accordion.Item>
-					),
-				)}
+				{schedule.map((day, i) => (
+					<div key={i}>
+						<h2 className={styles.date}>
+							{day[0].startTime.toLocaleDateString()}
+						</h2>
+						{day.map(
+							({ title, description, location, hosts, startTime, endTime }) => (
+								<Accordion.Item
+									key={title}
+									eventKey={title}
+									className={styles.accordionItem}
+								>
+									<Accordion.Header className={styles.accordionHeader}>
+										<h3 className={styles.eventTitle}>{title}</h3>
+										{/* <span>{hosts?.join()}</span> */}
+										<span className="text-end ms-auto">
+											{location},{" "}
+											{dateTimeFormat.formatRange(startTime, endTime)}
+										</span>
+									</Accordion.Header>
+									<Accordion.Body>{description}</Accordion.Body>
+								</Accordion.Item>
+							),
+						)}
+					</div>
+				))}
 			</Accordion>
 		</Container>
 	);
