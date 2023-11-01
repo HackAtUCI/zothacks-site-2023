1aef37959646b133c000039535ad074971dc8b1b
Sam Der <sder@uci.edu>
2023-10-31 20:14:42 -0700

    feat: frontend resources section

--- apps/site/src/views/Resources/Resources.tsx
+++ apps/site/src/views/Resources/Resources.tsx
@@ -2,12 +2,15 @@ import styles from "./Resources.module.scss";
 import Landing from "./sections/Landing/Landing";
 import ApiResources from "./sections/ApiResources/ApiResources";
 import BackendResources from "./sections/BackendResources/BackendResources";
+import FrontendResources from "./sections/FrontendResources/FrontendResources";
+
 export default function Resources() {
 	return (
 		<div className={styles.resources}>
 			<Landing />
 			<ApiResources />
 			<BackendResources />
+			<FrontendResources />
 		</div>
 	);
 }
