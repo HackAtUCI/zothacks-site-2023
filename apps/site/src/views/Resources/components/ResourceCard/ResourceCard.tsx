9214689ea4024afed3d90275c3faa634837a3088
Tyler Yu <tyleryy@uci.edu>
2023-11-02 06:35:55 -0700

    feature: animated sticky notes

--- apps/site/src/views/Resources/components/ResourceCard/ResourceCard.tsx
+++ apps/site/src/views/Resources/components/ResourceCard/ResourceCard.tsx
@@ -1,5 +1,6 @@
+"use client";
 import Image from "next/image";
-
+import { Variants, circIn, easeIn, motion } from "framer-motion";
 import openNewWindow from "@/assets/icons/open-new-window.svg";
 import styles from "./ResourceCard.module.scss";
 
@@ -16,6 +17,21 @@ interface ResourceCardProps {
 	stickyNoteColor: string;
 }
 
+const variant: Variants = {
+	initial: {
+		scale: 3,
+		opacity: 0,
+	},
+	animate: {
+		scale: 1,
+		opacity: 1,
+		transition: {
+			duration: 0.5,
+			staggerChildren: 0.1,
+		},
+	},
+};
+
 export default function ResourceCard({
 	title,
 	description,
@@ -24,38 +40,55 @@ export default function ResourceCard({
 	stickyNoteColor,
 }: ResourceCardProps) {
 	return (
-		<div
+		<motion.div
+			variants={variant}
+			initial="initial"
+			animate="animate"
 			className={styles.group}
 			style={{
 				backgroundColor: `${stickyNoteColor}`,
 			}}
 		>
-			<div className={styles.container + " text-center px-3"}>
-				<div className={styles.tape}></div>
-				{stickerSrc && <img src={stickerSrc} alt="Resource logo" width="100" />}
-				<h3>{title}</h3>
+			<motion.div
+				className={styles.container + " text-center px-3"}
+				variants={variant}
+			>
+				<motion.div className={styles.tape} variants={variant}></motion.div>
+				{stickerSrc && (
+					<motion.img
+						src={stickerSrc}
+						alt="Resource logo"
+						width="100"
+						variants={variant}
+					/>
+				)}
+				<motion.h3 variants={variant}>{title}</motion.h3>
 				{description}
-			</div>
+			</motion.div>
 
 			{links.map(({ text, link }) => (
-				<a
+				<motion.a
 					key={link}
 					href={link}
 					target="_blank"
 					rel="noopener noreferrer"
 					className={styles.tag}
+					variants={variant}
 				>
 					{text}
-					<div className="d-inline ms-1 vertical-align-middle">
+					<motion.div
+						className="d-inline ms-1 vertical-align-middle"
+						variants={variant}
+					>
 						<Image
 							src={openNewWindow}
 							width="20"
 							height="20"
 							alt="Open link in new window"
 						/>
-					</div>
-				</a>
+					</motion.div>
+				</motion.a>
 			))}
-		</div>
+		</motion.div>
 	);
 }
