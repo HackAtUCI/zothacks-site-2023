5d6a2d8e8d8fbf9f4680c6ccc272d3d376aac8c4
Alexander Liu <a@alexanderliu.com>
2023-11-02 23:08:12 -0700

    fix: timezone inconsistencies (#119)
    * fix: timezone inconsistencies
    
    * fix: timezone inconsistencies

--- apps/site/src/views/Schedule/sections/ClipboardSchedule/ClipboardSchedule.tsx
+++ apps/site/src/views/Schedule/sections/ClipboardSchedule/ClipboardSchedule.tsx
@@ -6,6 +6,7 @@ import Accordion from "react-bootstrap/Accordion";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import { utcToZonedTime } from "date-fns-tz";
 
 import clip from "@/assets/images/clip.svg";
 
@@ -50,37 +51,53 @@ function ClipboardSchedule({ schedule }: ClipboardScheduleProps) {
 			<Accordion defaultActiveKey="0" className={styles.accordion}>
 				{schedule.map((day, i) => (
 					<div className="mt-5" key={i}>
-						<h2>{weekdayFormat.format(day[0].startTime)}</h2>
+						<h2>
+							{weekdayFormat.format(
+								utcToZonedTime(day[0].startTime, "America/Los_Angeles"),
+							)}
+						</h2>
 						<p className="text-center mb-5 h3">
-							{monthDayFormat.format(day[0].startTime)}
+							{monthDayFormat.format(
+								utcToZonedTime(day[0].startTime, "America/Los_Angeles"),
+							)}
 						</p>
 						{day.map(
-							({ title, description, location, hosts, startTime, endTime }) => (
-								<Accordion.Item
-									key={title}
-									eventKey={title}
-									className={styles.accordionItem}
-								>
-									<Accordion.Header as="h3">
-										<Row className="w-100 align-items-center">
-											<Col lg>
-												<span className={styles.eventTitle + " h3 mb-0"}>
-													{title}
-												</span>
-											</Col>
-											{/* <span>{hosts?.join()}</span> */}
+							({ title, description, location, hosts, startTime, endTime }) => {
+								const startTimeZoned = utcToZonedTime(
+										startTime,
+										"America/Los_Angeles",
+									),
+									endTimeZoned = utcToZonedTime(endTime, "America/Los_Angeles");
+								return (
+									<Accordion.Item
+										key={title}
+										eventKey={title}
+										className={styles.accordionItem}
+									>
+										<Accordion.Header as="h3">
+											<Row className="w-100 align-items-center">
+												<Col lg>
+													<span className={styles.eventTitle + " h3 mb-0"}>
+														{title}
+													</span>
+												</Col>
+												{/* <span>{hosts?.join()}</span> */}
 
-											<Col lg className={styles.mobileLocation}>
-												<span>
-													{location},{" "}
-													{dateTimeFormat.formatRange(startTime, endTime)}
-												</span>
-											</Col>
-										</Row>
-									</Accordion.Header>
-									<Accordion.Body>{description}</Accordion.Body>
-								</Accordion.Item>
-							),
+												<Col lg className={styles.mobileLocation}>
+													<span>
+														{location},{" "}
+														{dateTimeFormat.formatRange(
+															startTimeZoned,
+															endTimeZoned,
+														)}
+													</span>
+												</Col>
+											</Row>
+										</Accordion.Header>
+										<Accordion.Body>{description}</Accordion.Body>
+									</Accordion.Item>
+								);
+							},
 						)}
 					</div>
 				))}
