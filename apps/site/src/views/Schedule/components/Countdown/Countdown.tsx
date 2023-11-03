56117a23c3485c09e57fbe105e43ca889b00949c
Sam Der <sder@uci.edu>
2023-11-03 01:26:48 -0700

    Setup animated countdown on schedule page (#93)
    * setup: basic framer countdown
    
    * fix: removed useRef, changed animation exit value
    
    * update: temporarily disabled slide up animation
    
    * fix: conditional day render
    
    * fix: precompute time
    
    * fix: margin for the countdown
    
    * update: added loading bar
    
    * fix: rename to Loader
    
    * feat: time until hacking countdown timer
    
    * fix: clipboard styling
    
    ---------
    
    Co-authored-by: Tyler Yu <tyleryy@uci.edu>
    Co-authored-by: Alexander Liu <a@alexanderliu.com>

---
+++ apps/site/src/views/Schedule/components/Countdown/Countdown.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import clsx from "clsx";
+import styles from "./Countdown.module.scss";
+
+// 10/4/23 10AM in UTC
+const hackingStarts = new Date(Date.UTC(2023, 10, 4, 17, 0, 0));
+
+export default function Countdown() {
+	const [remainingSeconds, setRemainingSeconds] = useState<number>(NaN);
+
+	useEffect(() => {
+		setRemainingSeconds(
+			(hackingStarts.valueOf() - new Date().valueOf()) / 1000,
+		);
+		const interval = setInterval(() => {
+			setRemainingSeconds((r) => r - 1);
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	return (
+		<div
+			className={clsx(
+				styles.countdown,
+				!isNaN(remainingSeconds) && styles.loaded,
+			)}
+		>
+			<span className={styles.time}>
+				<span className={styles.number}>
+					{Math.floor(remainingSeconds / (60 * 60))}
+				</span>
+				<span className={styles.colon}>:</span>
+				<span className={styles.number}>
+					{Math.floor((remainingSeconds / 60) % 60)
+						.toString()
+						.padStart(2, "0")}
+				</span>
+
+				<span className={styles.colon}>:</span>
+				<span className={styles.number}>
+					{Math.floor(remainingSeconds % 60)
+						.toString()
+						.padStart(2, "0")}
+				</span>
+			</span>
+			<span className={styles.caption}>Until Hacking Begins</span>
+		</div>
+	);
+}
