8b4a3e3ea1613ee4312eca57449c03d1a197f3b6
Tyler Yu <tyleryy@uci.edu>
2023-11-04 04:09:27 -0700

    update: mentor sticky note animation

--- apps/site/src/app/(home)/sections/Mentor/Mentor.tsx
+++ apps/site/src/app/(home)/sections/Mentor/Mentor.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { motion, cubicBezier, Variants } from "framer-motion";
 import Image from "next/image";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
@@ -10,6 +10,26 @@ import tape from "@/assets/images/tape.svg";
 import styles from "./Mentor.module.scss";
 
 const MENTOR_APP_URL = "/mentor";
+
+const variant: Variants = {
+	initial: {
+		scale: 1.1,
+		opacity: 0,
+		rotateX: 20,
+		translateY: 30,
+	},
+	animate: {
+		scale: 1,
+		rotateX: 0,
+		opacity: 1,
+		translateY: 0,
+		transition: {
+			duration: 0.85,
+			staggerChildren: 0.1,
+			ease: cubicBezier(0.33, 1, 0.68, 1),
+		},
+	},
+};
 
 const Mentor = () => {
 	const mentorHeader = (
@@ -35,20 +55,32 @@ const Mentor = () => {
 
 	return (
 		<Container as="section">
-			<div className="position-relative my-5">
-				<Col lg={5} className={styles.applySticky}>
-					<Image src={tape} alt="post-it tape" className={styles.tape} />
-					<div className={styles.applyStickyContent}>
-						{mentorHeader}
+			<motion.div
+				variants={variant}
+				initial="initial"
+				whileInView="animate"
+				className="position-relative my-5"
+			>
+				<motion.div variants={variant}>
+					<Col lg={5} className={styles.applySticky}>
+						<motion.img
+							variants={variant}
+							src={tape.src}
+							alt="post-it tape"
+							className={styles.tape}
+						/>
+						<div className={styles.applyStickyContent}>
+							{mentorHeader}
+							{mentorDescription}
+						</div>
+						{applyLink}
+					</Col>
+					<Col className={styles.descSticky + " text-center"}>
 						{mentorDescription}
-					</div>
-					{applyLink}
-				</Col>
-				<Col className={styles.descSticky + " text-center"}>
-					{mentorDescription}
-					{applyLink}
-				</Col>
-			</div>
+						{applyLink}
+					</Col>
+				</motion.div>
+			</motion.div>
 		</Container>
 	);
 };
