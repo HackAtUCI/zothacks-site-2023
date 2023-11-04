4fcaf4762ef65e21590c39408fe16ccd8c291aa9
Tyler Yu <tyleryy@uci.edu>
2023-11-04 03:51:17 -0700

    feat: pin animation

--- apps/site/src/app/(home)/sections/Intro/Intro.tsx
+++ apps/site/src/app/(home)/sections/Intro/Intro.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Container from "react-bootstrap/Container";
 
+import { motion, cubicBezier } from "framer-motion";
 import pin from "@/assets/images/index-card-pin.svg";
 import hackDoodle from "@/assets/images/hack-doodle.png";
 
@@ -12,9 +13,24 @@ const Intro = () => {
 	return (
 		<Container as="section">
 			<div className={styles.intro}>
-				<Image
+				<motion.img
+					initial={{
+						opacity: 0,
+						y: -10,
+						x: -10,
+					}}
+					viewport={{ once: true }}
+					whileInView={{
+						opacity: 1,
+						y: 0,
+						x: 0,
+						transition: {
+							duration: 0.65,
+							ease: cubicBezier(0.64, 0, 0.78, 0),
+						},
+					}}
 					className={styles.pin}
-					src={pin}
+					src={pin.src}
 					width="100"
 					height="100"
 					alt="Index card pin"
